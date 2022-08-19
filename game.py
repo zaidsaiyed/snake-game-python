b7098bcd052473cbce240726cf70c70f7d0952cc
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2022-08-18 23:46:26 -0400

    moving head of snake code added

--- game.py
+++ game.py
@@ -32,16 +32,36 @@ def initiate_map():
     file = "default.txt"
     path = current_directory+"\maps\\"
     
-    MAP = open(f'{path}{file}', "r")
+    MAP = open(f'{path}{file}', "r+")
+    MAP.seek(0)
     return MAP
 
 
 def scan_map(map):
     list_of_lines = map.read().split("\n")
     working_area = list_of_lines[1:-1]
+    map.seek(62)
+    map.write('O')
     # print(len(working_area[1]))
     # snake = '▒'
     # print(snake)
+    
+def move_snake(map, direction):
+    
+    v = range(34,63)
+    valid_pos=[]
+    
+    START_POS = 34
+    new_pos = START_POS
+    if direction == 'right':
+        # key press logic
+        while new_pos != 0:
+            map.seek(new_pos)
+            map.write('O')
+            new_pos += 1
+            if new_pos == 0:
+                pass
+    
         
     
 def print_map(map):
---
+++ test.py
@@ -0,0 +1,17 @@
+x = []
+#list = [2,4,6,8]
+#print(list)
+
+for num in range(0,361):
+	x.append(num)
+
+a = 34
+b = 63
+
+
+
+for _ in range(0,10):
+    print(x[a:b])
+    a+=33
+    b+=33
+    
