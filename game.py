2f572be78c605030c409a46fd7b45b1fa4327df0
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2022-08-20 23:58:35 -0400

    Added all directions

--- game.py
+++ game.py
@@ -90,6 +90,25 @@ def move_snake(map):
                     map.write(' ')
                     
                     new_pos += 1
+                    
+        if direction == 'a':
+            while direction == 'a':
+                if new_pos in valid_pos:
+                    direction, _ = timedInput("",0.3)
+                    if direction == '':
+                        direction = 'a'
+                    old_pos = new_pos
+                    map.seek(new_pos)
+                    map.write('O')
+                    
+                    os.system('cls')
+                    print_map(map)
+                    #sleep(0.25)
+                    
+                    map.seek(old_pos)
+                    map.write(' ')
+                    
+                    new_pos -= 1
                 
         if direction == 's':
             while direction == 's':
@@ -109,9 +128,26 @@ def move_snake(map):
                     map.write(' ')
                     
                     new_pos += 33
+                    
+        if direction == 'w':
+            while direction == 'w':
+                if new_pos in valid_pos:
+                    direction, _ = timedInput("",0.3)
+                    if direction == '':
+                        direction = 'w'
+                    old_pos = new_pos
+                    map.seek(new_pos)
+                    map.write('O')
+                    
+                    os.system('cls')
+                    print_map(map)
+                    #sleep(0.25)
+                                
+                    map.seek(old_pos)
+                    map.write(' ')
+                    
+                    new_pos -= 33
             
-    
-        
     
 def print_map(map):
     map.seek(0)
